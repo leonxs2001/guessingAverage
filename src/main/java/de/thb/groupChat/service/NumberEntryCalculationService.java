38c9a31f99477bc362e12d0000e4b85bd4f3e216
sxBUTCHxs <leonschoenberg@gmx.de>
2023-12-02 12:39:36 +0100

    Implement calculateMedian, getMaxNumber and getMinNumber of NumberEntryCalculationService.
    And change the order of expected and actual value in assertEquals.

--- src/main/java/de/thb/groupChat/service/NumberEntryCalculationService.java
+++ src/main/java/de/thb/groupChat/service/NumberEntryCalculationService.java
@@ -3,6 +3,8 @@ package de.thb.groupChat.service;
 import de.thb.groupChat.entities.NumberEntry;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -11,9 +13,43 @@ public class NumberEntryCalculationService {
         return 0;
     }
 
-    public float calculateMedian(List<NumberEntry> numberEntryList){return 0;}
+    public float calculateMedian(List<NumberEntry> numberEntryList){
+        if(numberEntryList == null || numberEntryList.size() == 0) {
+            return 0;
+        }
 
-    public float getMaxNumber(List<NumberEntry> numberEntryList){return 0;}
+        List<NumberEntry> copiedNumberEntries = new ArrayList<>(numberEntryList);
+        Collections.sort(copiedNumberEntries);
 
-    public float getMinNumber(List<NumberEntry> numberEntryList){return 0;}
+        if(copiedNumberEntries.size() % 2 == 0){
+            int rightMiddleIndex = copiedNumberEntries.size() / 2;
+
+            NumberEntry rightMiddleNumberEntry = copiedNumberEntries.get(rightMiddleIndex);
+            NumberEntry leftMiddleNumberEntry = copiedNumberEntries.get(rightMiddleIndex - 1);
+
+            float middleNumberEntryAverage = (rightMiddleNumberEntry.getNumber() + leftMiddleNumberEntry.getNumber()) / 2.0f;
+            return middleNumberEntryAverage;
+        }else{
+            NumberEntry middleNumberEntry = copiedNumberEntries.get(copiedNumberEntries.size() / 2);
+            return middleNumberEntry.getNumber();
+        }
+    }
+
+    public float getMaxNumber(List<NumberEntry> numberEntryList){
+        if(numberEntryList == null || numberEntryList.size() == 0) {
+            return 0;
+        }
+
+        NumberEntry maxNumberEntry = Collections.max(numberEntryList);
+        return maxNumberEntry.getNumber();
+    }
+
+    public float getMinNumber(List<NumberEntry> numberEntryList){
+        if(numberEntryList == null || numberEntryList.size() == 0) {
+            return 0;
+        }
+
+        NumberEntry minNumberEntry = Collections.min(numberEntryList);
+        return minNumberEntry.getNumber();
+    }
 }
--- src/test/java/de/thb/groupChat/GroupChatApplicationTests.java
+++ src/test/java/de/thb/groupChat/GroupChatApplicationTests.java
@@ -13,104 +13,104 @@ import java.util.List;
 @SpringBootTest
 class NumberEntryCalculationServiceTests {
 
-	@Autowired
-	private NumberEntryCalculationService numberEntryCalculationService;
+    @Autowired
+    private NumberEntryCalculationService numberEntryCalculationService;
 
-	@Test
-	void test_average_calculation_with_entries() {
-		List<NumberEntry> numberEntries = new ArrayList<>();
-		numberEntries.add(new NumberEntry(null, 1));
-		numberEntries.add(new NumberEntry(null, 3));
-		numberEntries.add(new NumberEntry(null, 10));
+    @Test
+    void test_average_calculation_with_entries() {
+        List<NumberEntry> numberEntries = new ArrayList<>();
+        numberEntries.add(new NumberEntry(null, 1));
+        numberEntries.add(new NumberEntry(null, 3));
+        numberEntries.add(new NumberEntry(null, 10));
 
-		Assertions.assertEquals(numberEntryCalculationService.calculateAverage(numberEntries), 7, "Given average is not the real average.");
-	}
+        Assertions.assertEquals(numberEntryCalculationService.calculateAverage(numberEntries), 7, "Given average is not the real average.");
+    }
 
-	@Test
-	public void test_average_calculation_without_entries(){
-		List<NumberEntry> numberEntries = new ArrayList<>();
-		Assertions.assertEquals(numberEntryCalculationService.calculateAverage(numberEntries), 0, "Given average should be 0 in case of no given entries.");
-	}
+    @Test
+    public void test_average_calculation_without_entries() {
+        List<NumberEntry> numberEntries = new ArrayList<>();
+        Assertions.assertEquals(numberEntryCalculationService.calculateAverage(numberEntries), 0, "Given average should be 0 in case of no given entries.");
+    }
 
-	@Test
-	public void test_average_calculation_with_null(){
-		Assertions.assertEquals(numberEntryCalculationService.calculateAverage(null), 0, "Given average should be 0 in case of null as given entries.");
-	}
+    @Test
+    public void test_average_calculation_with_null() {
+        Assertions.assertEquals(numberEntryCalculationService.calculateAverage(null), 0, "Given average should be 0 in case of null as given entries.");
+    }
 
-	@Test
-	void test_median_calculation_with_odd_number_of_entries() {
-		List<NumberEntry> numberEntries = new ArrayList<>();
-		numberEntries.add(new NumberEntry(null, 1));
-		numberEntries.add(new NumberEntry(null, 3));
-		numberEntries.add(new NumberEntry(null, 10));
+    @Test
+    void test_median_calculation_with_odd_number_of_entries() {
+        List<NumberEntry> numberEntries = new ArrayList<>();
+        numberEntries.add(new NumberEntry(null, 1));
+        numberEntries.add(new NumberEntry(null, 3));
+        numberEntries.add(new NumberEntry(null, 10));
 
-		Assertions.assertEquals(numberEntryCalculationService.calculateMedian(numberEntries), 3, "Given median is not the real median for an odd number of entries.");
-	}
+        Assertions.assertEquals(numberEntryCalculationService.calculateMedian(numberEntries), 3, "Given median is not the real median for an odd number of entries.");
+    }
 
-	@Test
-	void test_median_calculation_with_even_number_of_entries() {
-		List<NumberEntry> numberEntries = new ArrayList<>();
-		numberEntries.add(new NumberEntry(null, 1));
-		numberEntries.add(new NumberEntry(null, 3));
-		numberEntries.add(new NumberEntry(null, 4));
-		numberEntries.add(new NumberEntry(null, 10));
+    @Test
+    void test_median_calculation_with_even_number_of_entries() {
+        List<NumberEntry> numberEntries = new ArrayList<>();
+        numberEntries.add(new NumberEntry(null, 1));
+        numberEntries.add(new NumberEntry(null, 3));
+        numberEntries.add(new NumberEntry(null, 4));
+        numberEntries.add(new NumberEntry(null, 10));
 
-		Assertions.assertEquals(numberEntryCalculationService.calculateMedian(numberEntries), 3.5, "Given median is not the real median for an even number of entries. Should be the average of the 2 middle numbers.");
-	}
+        Assertions.assertEquals(3.5, numberEntryCalculationService.calculateMedian(numberEntries), "Given median is not the real median for an even number of entries. Should be the average of the 2 middle numbers.");
+    }
 
-	@Test
-	public void test_median_calculation_with_no_entries(){
-		List<NumberEntry> numberEntries = new ArrayList<>();
-		Assertions.assertEquals(numberEntryCalculationService.calculateMedian(numberEntries), 0, "Given median should be 0 in case of no given entries.");
-	}
+    @Test
+    public void test_median_calculation_with_no_entries() {
+        List<NumberEntry> numberEntries = new ArrayList<>();
+        Assertions.assertEquals(0, numberEntryCalculationService.calculateMedian(numberEntries), "Given median should be 0 in case of no given entries.");
+    }
 
-	@Test
-	public void test_median_calculation_with_null(){
-		Assertions.assertEquals(numberEntryCalculationService.calculateMedian(null), 0, "Given median should be 0 in case of null as given entries.");
-	}
+    @Test
+    public void test_median_calculation_with_null() {
+        Assertions.assertEquals(0, numberEntryCalculationService.calculateMedian(null), "Given median should be 0 in case of null as given entries.");
+    }
 
-	@Test
-	void test_max_number_with_entries() {
-		List<NumberEntry> numberEntries = new ArrayList<>();
-		numberEntries.add(new NumberEntry(null, 1));
-		numberEntries.add(new NumberEntry(null, 3));
-		numberEntries.add(new NumberEntry(null, 10));
+    @Test
+    void test_max_number_with_entries() {
+        List<NumberEntry> numberEntries = new ArrayList<>();
+        numberEntries.add(new NumberEntry(null, 1));
+        numberEntries.add(new NumberEntry(null, 3));
+        numberEntries.add(new NumberEntry(null, 10));
 
-		Assertions.assertEquals(numberEntryCalculationService.getMaxNumber(numberEntries), 10, "Given max value is not the real max value.");
-	}
+        Assertions.assertEquals(10, numberEntryCalculationService.getMaxNumber(numberEntries), "Given max value is not the real max value.");
+    }
 
-	@Test
-	void test_max_number_with_no_entries() {
-		List<NumberEntry> numberEntries = new ArrayList<>();
+    @Test
+    void test_max_number_with_no_entries() {
+        List<NumberEntry> numberEntries = new ArrayList<>();
 
-		Assertions.assertEquals(numberEntryCalculationService.getMaxNumber(numberEntries), 0, "Given max value should be 0 in case of no given entries.");
-	}
+        Assertions.assertEquals(0, numberEntryCalculationService.getMaxNumber(numberEntries), "Given max value should be 0 in case of no given entries.");
+    }
 
-	@Test
-	void test_max_number_with_null() {
-		Assertions.assertEquals(numberEntryCalculationService.getMaxNumber(null), 0, "Given max value should be 0 in case of null as given entries.");
-	}
+    @Test
+    void test_max_number_with_null() {
+        Assertions.assertEquals(0, numberEntryCalculationService.getMaxNumber(null), "Given max value should be 0 in case of null as given entries.");
+    }
 
-	@Test
-	void test_min_number_with_entries() {
-		List<NumberEntry> numberEntries = new ArrayList<>();
-		numberEntries.add(new NumberEntry(null, 1));
-		numberEntries.add(new NumberEntry(null, 3));
-		numberEntries.add(new NumberEntry(null, 10));
+    @Test
+    void test_min_number_with_entries() {
+        List<NumberEntry> numberEntries = new ArrayList<>();
+        numberEntries.add(new NumberEntry(null, 1));
+        numberEntries.add(new NumberEntry(null, 3));
+        numberEntries.add(new NumberEntry(null, 10));
 
-		Assertions.assertEquals(numberEntryCalculationService.getMinNumber(numberEntries), 1, "Given min value is not the real min value.");
-	}
+        Assertions.assertEquals(1, numberEntryCalculationService.getMinNumber(numberEntries), "Given min value is not the real min value.");
+    }
 
-	@Test
-	void test_min_number_with_no_entries() {
-		List<NumberEntry> numberEntries = new ArrayList<>();
+    @Test
+    void test_min_number_with_no_entries() {
+        List<NumberEntry> numberEntries = new ArrayList<>();
 
-		Assertions.assertEquals(numberEntryCalculationService.getMinNumber(numberEntries), 0, "Given min value should be 0 in case of no given entries.");
-	}
+        Assertions.assertEquals(0, numberEntryCalculationService.getMinNumber(numberEntries), "Given min value should be 0 in case of no given entries.");
+    }
 
-	@Test
-	void test_min_number_with_null() {
-		Assertions.assertEquals(numberEntryCalculationService.getMinNumber(null), 0, "Given min value should be 0 in case of null as given entries.");
-	}
+    @Test
+    void test_min_number_with_null() {
+        Assertions.assertEquals(0, numberEntryCalculationService.getMinNumber(null), "Given min value should be 0 in case of null as given entries.");
+    }
 
 }
