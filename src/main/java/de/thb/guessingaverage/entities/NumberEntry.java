82cba7be4ed249759a27d9704b072dd5151d9b76
einToast <laurin2102@gmail.com>
2023-12-14 05:19:25 +0100

    Add POST method: - Add NumberEntryFormModel and implement addNumber method - Add Output of Average in guessing-average.html

--- src/main/java/de/thb/guessingaverage/entities/NumberEntry.java
+++ src/main/java/de/thb/guessingaverage/entities/NumberEntry.java
@@ -1,6 +1,5 @@
 package de.thb.guessingaverage.entities;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
